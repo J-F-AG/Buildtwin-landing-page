1888ecad867a3eca3f0d5ce1f46a4ee4792ae4d5
Nitesh <nitesh@code5.org>
2024-02-07 17:06:29 +0530

    Implemented markup of the training page.

--- blin/src/app/components/common/about/fs-about/fs-about.component.ts
+++ blin/src/app/components/common/about/fs-about/fs-about.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { OwlOptions } from 'ngx-owl-carousel-o';
 
 @Component({
   selector: 'app-fs-about',
@@ -6,5 +7,17 @@ import { Component } from '@angular/core';
   styleUrls: ['./fs-about.component.scss']
 })
 export class FsAboutComponent {
-
+  aboutSlider: OwlOptions = {
+    items: 1,
+    nav: true,
+    margin: 0,
+    dots: true,
+    loop: true,
+    autoplay: true,
+    autoplayHoverPause: true,
+    navText: [
+      "<i class='ti ti-chevron-left'></i>",
+      "<i class='ti ti-chevron-right'></i>",
+    ]
+  }
 }
