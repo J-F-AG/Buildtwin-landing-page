de43f3144ea7754d24ad911b4b4a2ec510bf5eea
Nitesh <nitesh@code5.org>
2024-02-08 22:54:21 +0530

    UI adjustment.

--- blin/src/app/components/common/integrations/hd-integrations/hd-integrations.component.ts
+++ blin/src/app/components/common/integrations/hd-integrations/hd-integrations.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { OwlOptions } from 'ngx-owl-carousel-o';
 
 @Component({
   selector: 'app-hd-integrations',
@@ -6,5 +7,33 @@ import { Component } from '@angular/core';
   styleUrls: ['./hd-integrations.component.scss']
 })
 export class HdIntegrationsComponent {
-
+  integrationsList: OwlOptions = {
+    nav: false,
+    margin: 25,
+    loop: false,
+    dots: false,
+    autoplay: true,
+    autoplayHoverPause: true,
+    navText: [
+      "<i class='ti ti-chevron-left'></i>",
+      "<i class='ti ti-chevron-right'></i>",
+    ],
+    responsive: {
+      0: {
+        items: 2
+      },
+      515: {
+        items: 3
+      },
+      768: {
+        items: 4
+      },
+      990: {
+        items: 5
+      },
+      1400: {
+        items: 6
+      }
+    }
+  }
 }
--- blin/src/app/components/pages/fintech-saas-home/fs-banner/fs-banner.component.ts
+++ blin/src/app/components/pages/fintech-saas-home/fs-banner/fs-banner.component.ts
@@ -1,8 +1,20 @@
 import { Component } from '@angular/core';
+import { OwlOptions } from 'ngx-owl-carousel-o';
 
 @Component({
 	selector: 'app-fs-banner',
 	templateUrl: './fs-banner.component.html',
 	styleUrls: ['./fs-banner.component.scss']
 })
-export class FsBannerComponent {}
+export class FsBannerComponent {
+	cardSlider: OwlOptions = {
+		items: 5,
+		nav: false,
+		margin: -90,
+		loop: true,
+		dots: false,
+        center: true,
+		autoplay: true,
+		autoplayHoverPause: true
+    }
+}
