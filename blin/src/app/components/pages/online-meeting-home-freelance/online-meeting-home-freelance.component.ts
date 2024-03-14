479ec5b43675b8558c12ee397bbf581365d55e54
ashish yadav <ashish.y@ind.jf.ag>
2024-03-14 19:40:49 +0530

    ui changes

--- blin/src/app/components/pages/online-meeting-home-freelance/online-meeting-home-freelance.component.ts
+++ blin/src/app/components/pages/online-meeting-home-freelance/online-meeting-home-freelance.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 import { Title } from '@angular/platform-browser';
 
 @Component({
@@ -9,11 +9,73 @@ import { Title } from '@angular/platform-browser';
 export class OnlineMeetingHomeFreelanceComponent {
 
     title = 'BuildTwin - Software for Freelancer / small Teams';
- 
+    activeState: number = 1
+    FixedDiv: any
+    FixedDivPositionTop: any
+    fixedDIvOffsetTop: any
+    scrollPosition: any
+  
+    // scrolled div 
+  
+    scrollActivated: any
+    scrollDivOffsettop: any
+    scrolledDivHeight: any
+    fixedElement: any
+
     constructor(private titleService:Title) {}
     
     ngOnInit() {
         this.titleService.setTitle(this.title);
-    }
 
+        this.scrollActivated = document.getElementById('scrollActivated');
+        setTimeout(() => {
+          this.scrollDivOffsettop = this.scrollActivated.getBoundingClientRect().top
+          this.scrolledDivHeight = this.scrollActivated.getBoundingClientRect().height
+        }, 2000);
+    }
+    @HostListener('window:scroll', ['$event'])
+    handleScroll(event: any) {
+      this.FixedDiv = document.getElementById('scrollActivated');
+      this.FixedDiv = this.FixedDiv.getBoundingClientRect().top;
+      let topscroll = this.scrollDivOffsettop - this.FixedDiv
+      let winH = window.innerHeight
+      let totalScroll = Number(this.scrollDivOffsettop + this.scrolledDivHeight) - winH
+      this.fixedElement = document.getElementById('scrollActivated');
+  
+      // inside active 
+      if (this.FixedDiv < 200 && totalScroll > topscroll) {
+        this.fixedElement.classList.add("fixed")
+        let activeELe = document.querySelectorAll('[data-ele]');
+        activeELe.forEach((item, index) => {
+          if (item.getBoundingClientRect().top < 500) {
+            this.activeState = index + 1;
+            item.classList.add("active")
+          }
+          else {
+            item.classList.remove("active")
+          }
+  
+        })
+  
+      }
+      else {
+        this.fixedElement.classList.remove("fixed")
+  
+      }
+    }
+  
+    scrollToSection(sectionId: string) {
+      const section = document.getElementById(sectionId);
+      if (section) {
+        // Scroll the section into view smoothly
+        section.scrollIntoView({ behavior: 'smooth', block: 'start' });
+        
+        // Adjust scroll position to maintain a 100-pixel gap from the top of the viewport
+        setTimeout(() => {
+          const offsetTop = section.getBoundingClientRect().top;
+          const desiredOffset = offsetTop - 390; // Adjust the desired offset as needed
+          window.scrollBy(0, desiredOffset);
+        }, 100); // Adjust the delay if needed
+      }
+    }
 }
