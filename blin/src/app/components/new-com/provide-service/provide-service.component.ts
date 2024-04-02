1ca8ecc06b02bbee99e1b5666c2683bfce96478d
ashish yadav <ashish.y@ind.jf.ag>
2024-04-02 17:03:24 +0530

    deisng chnages

--- blin/src/app/components/new-com/provide-service/provide-service.component.ts
+++ blin/src/app/components/new-com/provide-service/provide-service.component.ts
@@ -12,23 +12,15 @@ export class ProvideServiceComponent {
   activeState: number = 1
   activeState2: number = 11
   title = 'BuildTwin - Software for technical Teams';
-
-
   activeState3: number = 21
   FixedDiv: any
   FixedDivPositionTop: any
   fixedDIvOffsetTop: any
-
   fixedElement3: any
   scrollPosition: any
 
-
-
-
   FixedDiv2: any
-
   // scrolled div 
-
   scrollActivated: any
   scrollDivOffsettop: any
   scrolledDivHeight: any
@@ -36,24 +28,18 @@ export class ProvideServiceComponent {
   fixedElement1: any
 
   constructor(private titleService: Title, private router: Router) {
-
     router.events.subscribe((val) => {
       this.scrollActivated = document.getElementById('scrollActivated');
       setTimeout(() => {
         this.scrollDivOffsettop = this.scrollActivated.getBoundingClientRect().top
         this.scrolledDivHeight = this.scrollActivated.getBoundingClientRect().height
         console.log(this.scrollDivOffsettop, this.scrolledDivHeight);
-
       }, 2000);
     });
   }
 
   ngOnInit() {
-
-
     this.titleService.setTitle(this.title);
-
-
   }
   @HostListener('window:scroll', ['$event'])
   handleScroll(event: any) {
@@ -63,7 +49,6 @@ export class ProvideServiceComponent {
     let winH = window.innerHeight
     let totalScroll = Number(this.scrollDivOffsettop + this.scrolledDivHeight) - 300
     this.fixedElement = document.getElementById('scrollActivated');
-
     // inside active 
     if (this.FixedDiv < 200 && totalScroll > topscroll + 200) {
       this.fixedElement.classList.add("fixed2")
@@ -76,12 +61,7 @@ export class ProvideServiceComponent {
         else {
           item.classList.remove("active")
         }
-
-
-
         // /////////////
-
-
         if (this.activeState === 2) {
           this.fixedElement1 = document.getElementById('scrollActivated2');
 
@@ -97,7 +77,7 @@ export class ProvideServiceComponent {
                 item1.classList.add("active");
                 const dataIndex = item1.getAttribute(`data-ele${i}`);
                 if (dataIndex) {
-                  this.activeState2 = parseInt(dataIndex, 10); // Assuming dataIndex is a number
+                  this.activeState2 = parseInt(dataIndex, 10);
                 }
               } else {
                 item1.classList.remove("active");
@@ -110,29 +90,21 @@ export class ProvideServiceComponent {
           this.fixedElement1.classList.remove("fixed")
         }
 
-
-
         if (this.activeState === 3) {
           this.fixedElement3 = document.getElementById('scrollActivated3');
-
           if (this.fixedElement3) {
             this.fixedElement3.classList.add("fixedData");
           }
-
           for (let j = 21; j <= 24; j++) {
-
             let activeELe3 = document.querySelectorAll(`[data-element${j}]`);
             activeELe3.forEach((item3) => {
-
               const topPosition = item3.getBoundingClientRect().top;
-              if (topPosition < 500) {
+              if (topPosition < 200) {
                 item3.classList.add("active");
                 const dataIndex = item3.getAttribute(`data-element${j}`);
                 if (dataIndex) {
-                  this.activeState3 = parseInt(dataIndex); // Assuming dataIndex is a number
+                  this.activeState3 = parseInt(dataIndex);
                 }
-
-                
               } else {
                 item3.classList.remove("active");
               }
@@ -143,8 +115,6 @@ export class ProvideServiceComponent {
           this.fixedElement3 = document.getElementById('scrollActivated3');
           this.fixedElement3.classList.remove("fixedData")
         }
-
-
       })
 
     }
