814cbbb781b9d7fc2f29514f36c41e7d8f8f5c54
ashish yadav <ashish.y@ind.jf.ag>
2024-04-04 16:37:56 +0530

    ui changes

--- blin/src/app/components/pages/email-marketing-home/email-marketing-home.component.ts
+++ blin/src/app/components/pages/email-marketing-home/email-marketing-home.component.ts
@@ -1,5 +1,6 @@
 import { Component, HostListener } from '@angular/core';
 import { Title } from '@angular/platform-browser';
+import { Router } from '@angular/router';
 import { GlobalService } from 'src/app/services/GlobalService';
 
 @Component({
@@ -31,7 +32,18 @@ export class EmailMarketingHomeComponent {
   
     title = 'BuildTwin - good reasons';
  
-    constructor(private titleService:Title,private globalService: GlobalService) {}
+    constructor(private titleService:Title,private globalService: GlobalService,private router: Router) {
+
+      router.events.subscribe((val) => {
+        this.scrollActivated = document.getElementById('scrollActivated');
+        setTimeout(() => {
+          this.scrollDivOffsettop = this.scrollActivated.getBoundingClientRect().top
+          this.scrolledDivHeight = this.scrollActivated.getBoundingClientRect().height
+          console.log(this.scrollDivOffsettop,this.scrolledDivHeight);
+          
+        }, 2000);
+    });
+    }
     
 
   getGlobalValue(): string {
@@ -50,94 +62,25 @@ export class EmailMarketingHomeComponent {
       let winH = window.innerHeight
       let totalScroll = Number(this.scrollDivOffsettop + this.scrolledDivHeight) - 300
       this.fixedElement = document.getElementById('scrollActivated');
+  
       // inside active 
       if (this.FixedDiv < 200 && totalScroll > topscroll + 200) {
-        this.fixedElement.classList.add("fixed2")
-  
+        this.fixedElement.classList.add("fixedData")
         let activeELe = document.querySelectorAll('[data-ele]');
         activeELe.forEach((item, index) => {
           if (item.getBoundingClientRect().top < 500) {
             this.activeState = index + 1;
             item.classList.add("active")
-  
-         
           }
           else {
             item.classList.remove("active")
-           
-          }
-          // /////////////
-          if (this.activeState === 2) {
-            this.fixedElement1 = document.getElementById('scrollActivated2');
-  
-            if (this.fixedElement1) {
-              this.fixedElement1.classList.add("fixed");
-            }
-  
-            for (let i = 11; i <= 16; i++) {
-              let activeELe1 = document.querySelectorAll(`[data-ele${i}]`);
-              activeELe1.forEach((item1) => {
-                const topPosition = item1.getBoundingClientRect().top;
-                if (topPosition < 500) {
-                  item1.classList.add("active");
-                  const dataIndex = item1.getAttribute(`data-ele${i}`);
-                  if (dataIndex) {
-                    this.activeState2 = parseInt(dataIndex, 10);
-                  }
-                } else {
-                  item1.classList.remove("active");
-                }
-              });
-            }
-          }
-          else {
-            this.fixedElement1 = document.getElementById('scrollActivated2');
-            this.fixedElement1.classList.remove("fixed")
           }
   
-          if (this.activeState === 3) {
-  
-            this.navBar = document.getElementById('navBar');
-            this.navBar.classList.add("removeMainNav")
-            
-  
-            this.tabHead = document.getElementById('tabHead');
-            this.tabHead.classList.add("removeMainNav")
-  
-  
-            this.fixedElement3 = document.getElementById('scrollActivated3');
-            if (this.fixedElement3) {
-              this.fixedElement3.classList.add("fixedData");
-            }
-            for (let j = 21; j <= 24; j++) {
-              let activeELe3 = document.querySelectorAll(`[data-element${j}]`);
-              activeELe3.forEach((item3) => {
-                const topPosition = item3.getBoundingClientRect().top;
-                if (topPosition < 200) {
-                  item3.classList.add("active");
-                  const dataIndex = item3.getAttribute(`data-element${j}`);
-                  if (dataIndex) {
-                    this.activeState3 = parseInt(dataIndex);
-                  }
-                } else {
-                  item3.classList.remove("active");
-                }
-              });
-            }
-          }
-          else {
-            this.fixedElement3 = document.getElementById('scrollActivated3');
-            this.fixedElement3.classList.remove("fixedData")
-            this.navBar?.classList.remove("removeMainNav")
-            this.tabHead?.classList.remove("removeMainNav")
-          }
         })
   
       }
       else {
-        this.fixedElement.classList.remove("fixed2")
-  
-       
+        this.fixedElement.classList.remove("fixedData")
   
       }
     }
