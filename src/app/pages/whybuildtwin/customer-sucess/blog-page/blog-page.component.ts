c50ee1cfd313ef432ab96fa868dc2152ec77f554
dharmendra bisht <dharmendra@code5.org>
2024-09-17 16:36:12 +0530

    route added

--- src/app/pages/whybuildtwin/customer-sucess/blog-page/blog-page.component.ts
+++ src/app/pages/whybuildtwin/customer-sucess/blog-page/blog-page.component.ts
@@ -1,7 +1,9 @@
+import { ViewportScroller } from '@angular/common';
 import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
 import { Title } from '@angular/platform-browser';
-import { Router } from '@angular/router';
+import { NavigationEnd, Router } from '@angular/router';
+import { filter } from 'rxjs';
 import { LanguageService } from 'src/app/services/language.service';
 
 @Component({
@@ -13,7 +15,7 @@ export class BlogPageComponent {
 
     title = 'BuildTwin - use cases';
  
-    constructor(private titleService:Title, public _languageService:LanguageService) {
+    constructor(private titleService:Title, public _languageService:LanguageService, private router: Router, private viewportScroller: ViewportScroller) {
 
     }
     
@@ -21,6 +23,17 @@ export class BlogPageComponent {
         this.titleService.setTitle(this.title);
     }
 
-    
+    ngAfterViewInit(): void {
+        this.router.events.pipe(
+          filter((event): event is NavigationEnd => event instanceof NavigationEnd)
+        ).subscribe((event: NavigationEnd) => {
+          const fragment = this.router.url.split('#')[1];
+          if (fragment) {
+            setTimeout(() => {
+              this.viewportScroller.scrollToAnchor(fragment);
+            }, 500); // Delay to wait for content to load
+          }
+        });
+      }
 
 }
