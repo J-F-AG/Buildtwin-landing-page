18dc0e1f7de6781e091b57e09786024009861cdd
Harshit Agarwal <harshitagarwal@ind.jf.ag>
2024-10-25 12:16:46 +0530

    resolved the bugs

--- src/app/pages/co-pilot-for/digital-self-management/om-about/om-about.component.ts
+++ src/app/pages/co-pilot-for/digital-self-management/om-about/om-about.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ElementRef, Renderer2 } from '@angular/core';
 import { LanguageService } from 'src/app/services/language.service';
 
 @Component({
@@ -6,8 +6,31 @@ import { LanguageService } from 'src/app/services/language.service';
   templateUrl: './om-about.component.html',
   styleUrls: ['./om-about.component.scss']
 })
-export class OmAboutComponent {
-  constructor(public _languageService:LanguageService,) { }
+export class OmAboutComponent implements OnInit {
+  constructor(public _languageService:LanguageService,  private elRef: ElementRef, private renderer: Renderer2) { }
   
+  showPopup = false;
+
+  ngOnInit(): void {
+    // this.loadScript();
+    // console.log("inside ngOnInit, below this.loadScript() ");
+  }
+
+  //   loadScript() {
+  //   // Create script element
+  //   const script = this.renderer.createElement('script');
+  //   script.type = 'text/javascript';
+  //   script.src = 'https://static.hsappstatic.net/MeetingsEmbed/ex/MeetingsEmbedCode.js';
+
+  //   // Append script element to the body
+  //   this.renderer.appendChild(this.elRef.nativeElement, script);
+  // }
+
+  call(){
+    this.showPopup =true
+    }
+    closePopupStatus($event) {
+      this.showPopup = false;
+    }
 
 }
