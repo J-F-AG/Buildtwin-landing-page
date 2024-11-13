d38dbaff1a7df53da964f72c40f8b914487a8a7d
dharmendra bisht <dharmendra@code5.org>
2024-11-13 13:59:46 +0530

    build issue resolved.

--- src/app/pages/bim-usa/banner/banner.component.ts
+++ src/app/pages/bim-usa/banner/banner.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit, OnDestroy  } from '@angular/core';
+import { Component, OnInit, OnDestroy, Inject, PLATFORM_ID  } from '@angular/core';
 import { interval, Subscription } from 'rxjs';
+import { isPlatformBrowser } from '@angular/common';
 
 @Component({
   selector: 'app-bim-usa-banner',
@@ -13,13 +14,17 @@ export class BimUsaBannerComponent {
   cities = ['Arizona', 'New York', 'Texas', 'Pennsylvania', 'Florida', 'California'];
   currentCity: string = this.cities[0];
   private cityIndex = 0;
-  private citySubscription: Subscription;
+  private citySubscription: Subscription | undefined;
+
+  constructor(@Inject(PLATFORM_ID) private platformId: Object) {}
 
   ngOnInit(): void {
-    this.citySubscription = interval(2000).subscribe(() => {
-      this.cityIndex = (this.cityIndex + 1) % this.cities.length; // Loop back to the first city
-      this.currentCity = this.cities[this.cityIndex];
-    });
+    if (isPlatformBrowser(this.platformId)) {
+      this.citySubscription = interval(2000).subscribe(() => {
+        this.cityIndex = (this.cityIndex + 1) % this.cities.length; // Loop back to the first city
+        this.currentCity = this.cities[this.cityIndex];
+      });
+    }
   }
 
   ngOnDestroy(): void {
