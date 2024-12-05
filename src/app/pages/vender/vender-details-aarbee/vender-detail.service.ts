87c5ae8aba70e15aa928453e72fd31566e5d98b4
dharmendra bisht <dharmendra@code5.org>
2024-12-05 09:17:18 +0530

    code updated

--- src/app/pages/vender/vender-details-aarbee/vender-detail.service.ts
+++ src/app/pages/vender/vender-details-aarbee/vender-detail.service.ts
@@ -5,7 +5,7 @@ import { Injectable } from '@angular/core';
 })
 export class VenderDetailService {
   detalMeta = {
-    'j&findia': {
+    'j-f-india': {
       title: 'J&F India | Precast Design and Detailing Specialists | Buildtwin Partner',
       description: "J&F India is an internationally established precast design and detailing specialists and partner of Buildtwin. We offer a wide range of services including PEB Design and Detailing, BoQ/QS Services, Connection Engineering, and much more. Trust J&F India for all your engineering and project management needs.",
       image: 'https://www.buildtwin.com/assets/images/buildtwin.jpg',
@@ -17,79 +17,79 @@ export class VenderDetailService {
       image: 'https://www.buildtwin.com/assets/images/buildtwin.jpg',
       canonical: 'https://www.buildtwin.com/partners/durchgeplant'
     },
-    'caldim': {
+    'caldim-engineering': {
       title: 'Caldim | Engineering and Detailing Services | Buildtwin Partner',
       description: "Caldim offers top-notch engineering and detailing services, specializing in steel detailing, precast detailing, connection design, BIM, architecture, and more. As a trusted Buildtwin partner, we help bring your projects to life with precision and expertise.",
       image: 'https://www.buildtwin.com/assets/images/buildtwin.jpg',
       canonical: 'https://www.buildtwin.com/partners/caldim-engineering'
     },
-    'sublime': {
+    'sublime-structures': {
       title: 'Sublime Structures | Structural Detailing & Designing Solutions | Buildtwin Partner',
       description: "Sublime Structures is a leading provider of professional structural detailing and designing solutions. Our expert team caters to clients in the United States, Canada, and the United Kingdom, and beyond. Partner with Buildtwin for all your construction modeling needs.",
       image: 'https://www.buildtwin.com/assets/images/buildtwin.jpg',
       canonical: 'https://www.buildtwin.com/partners/sublime-structures'
     },
-    'mold-tektechnologieslimited': {
+    'mold-tek-technologies-limited': {
       title: 'Moldtek Technologies | Engineering & Technology Solutions | Buildtwin Partner',
       description: "Moldtek Technologies is your trusted partner for engineering and technology solutions. We specialize in manufacturing design, construction, telecom, and more. Let us help bring your ideas to life. ",
       image: 'https://www.buildtwin.com/assets/images/buildtwin.jpg',
       canonical: 'https://www.buildtwin.com/partners/mold-tek-technologies-limited'
     },
-    'gbcengineers': {
+    'gbc-engineers': {
       title: 'GBC Engineers |  Structural Engineering & Design Solutions | Buildtwin Partner',
       description: "GBC Engineers is a leading provider of high-quality structural engineering and design solutions for projects worldwide. Our expertise includes civil engineering, geotechnical engineering, value engineering design, and project management services.",
       image: 'https://www.buildtwin.com/assets/images/buildtwin.jpg',
       canonical: 'https://www.buildtwin.com/partners/gbc-engineers'
     },
-    'aarbeestructuresprivatelimited': {
+    'aarbee-structures-private-limited': {
       title: 'Aarbee Structures | Structural Steel Designing & Detailing | Buildtwin Partner',
       description: "Aarbee Structures is a trusted partner for all your structural design and steel detailing needs in residential, commercial, and industrial projects. Work with us and experience top-notch services from Buildtwin.",
       image: 'https://www.buildtwin.com/assets/images/buildtwin.jpg',
       canonical: 'https://www.buildtwin.com/partners/aarbee-structures-private-limited'
     },
-    'trikaaltechEnterprisesprivatelimited': {
+    'trikaal-tech-enterprises-private-limited': {
       title: 'Trikaal Tech Enterprises | BIM/VDC, Engineering Design Services | Buildtwin Partner',
       description: "Trikaal Tech Enterprises is a premier BIM/VDC and engineering design services provider, offering comprehensive solutions in the telecom, construction, automotive, and solar energy industries. As a trusted Buildtwin Partner, we delivers innovative, tailored, and high-quality engineering solutions.",
       image: 'https://www.buildtwin.com/assets/images/buildtwin.jpg',
       canonical: 'https://www.buildtwin.com/partners/trikaal-tech-enterprises-private-limited'
     },
-    'replinfosystems': {
+    'repl-infosystems': {
       title: 'REPL infosystems | Detailed Engineering, BIM Modelling, MEP | Buildtwin Partner',
       description: "REPL infosystems is a trusted partner for detailed engineering, BIM modeling, and MEP services in the AEC and Mechanical sectors. Our expertise includes architecture software, construction project management, BIM, 3D modeling software.",
       image: 'https://www.buildtwin.com/assets/images/buildtwin.jpg',
       canonical: 'https://www.buildtwin.com/partners/repl-infosystems'
     },
-    'precisionprecastsolutionsprivatelimited': {
+    'precision-precast-solutions-private-limited': {
       title: 'Precision Precast Solutions | Civil/Structural, MEP Engineering Services | Buildtwin Partner',
       description: "Precision Precast Solutions is a trusted partner for civil/structural and MEP engineering services. Our services includes design and detailing, BIM modeling, MEP, project management, and precast detailing. Partner with us for precision and efficient precast solutions.",
       image: 'https://www.buildtwin.com/assets/images/buildtwin.jpg',
       canonical: 'https://www.buildtwin.com/partners/precision-precast-solutions-private-limited'
     },
-    'orioledesigntechsolutionsllp': {
+    'oriole-design-tech-solutions-llp': {
       title: 'Oriole | BIM Solutions For AEC Industry | Buildtwin Partner',
       description: "Oriole is a leading BIM service provider offering end-to-end solutions to architects, engineers, and construction professionals in the AEC industry. Partnered with Buildtwin, we deliver BIM solutions to streamline your projects and enhance collaboration.",
       image: 'https://www.buildtwin.com/assets/images/buildtwin.jpg',
       canonical: 'https://www.buildtwin.com/partners/oriole-design-tech-solutions-llp'
     },
-    'innowellengineeringinternationalpvtltd': {
+    'innowell-engineering-international-pvt-ltd': {
       title: 'Innowell Engineering  | MEP, BIM & Sustainability Services | Buildtwin Partner',
       description: "Innowell Engineering offers MEP, BIM, and sustainability services as a Buildtwin Partner. We provide expert solutions in structural steel detailing, BIM modelling, and MEP design services for commercial buildings, industries, data centers.",
       image: 'https://www.buildtwin.com/assets/images/buildtwin.jpg',
       canonical: 'https://www.buildtwin.com/partners/innowell-engineering-international-pvt-ltd'
     },
-    'clovetechnologiesprivatelimited': {
+    'clove-technologies-private-limited': {
       title: 'Clove Technologies | BIM Modelling for AEC Industries | Buildtwin Partner',
       description: "Clove Technologies is a leading BIM modeling service provider specializing in the AEC industry, partnering with Buildtwin. Our comprehensive expertise includes design, detailing, digital construction, mapping, measurement, and transformation services.",
       image: 'https://www.buildtwin.com/assets/images/buildtwin.jpg',
       canonical: 'https://www.buildtwin.com/partners/clove-technologies-private-limited'
     },
-    'tritechcorporation': {
+    'tritech-corporation': {
       title: 'TRITECH ENGINEERING | BIM & MEP Engineering Solutions | Buildtwin Partner',
       description: "Tritech Engineering is a leading firm specializing in MEP solutions and integrated BIM modeling. As a trusted Buildtwin partner, we deliver innovative engineering solutions for your construction projects.",
       image: 'https://www.buildtwin.com/assets/images/buildtwin.jpg',
       canonical: 'https://www.buildtwin.com/partners/tritech-corporation'
     },
-    'cosmeretechnologies': {
+    'cosmere-technologies': {
       title: 'Cosmere Technologies | Digital Engineering & Modeling Services | Buildtwin Partner',
       description: "Cosmere Technologies specializes in digital engineering and modeling services for the AEC and infrastructure industries. As a trusted Buildtwin partner, we offer comprehensive digitization solutions including BIM, geospatial, CGI, and digitization services.",
       image: 'https://www.buildtwin.com/assets/images/buildtwin.jpg',
