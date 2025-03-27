657df4f4f054dd6e8b3da66997e001aacf32a32b
jf-dharmendrabisht <dharmendra@code5.org>
2025-03-27 10:17:01 +0530

    for buyer and for seller page UI updated

--- src/app/pages/marketplace/marketPlace/tt-banner/tt-banner.component.ts
+++ src/app/pages/marketplace/marketPlace/tt-banner/tt-banner.component.ts
@@ -4,7 +4,7 @@ import { LanguageService } from 'src/app/services/language.service';
 @Component({
   selector: 'app-tt-banner',
   templateUrl: './tt-banner.component.html',
-  styleUrls: ['../../../browse-services/browse-services-main/browse-services-main.component.scss','./tt-banner.component.scss']
+  styleUrls: ['./tt-banner.component.scss']
 })
 export class TtBannerComponent {
   showPopup=false;
