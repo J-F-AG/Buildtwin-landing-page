96539a3b6163037fbf9fe97a1defaf86cc22bc5f
jfpit <100768317+jfpit@users.noreply.github.com>
2025-08-27 10:58:31 +0200

    Content

--- src/app/pages/Home/heavy-lifting/heavy-lifting.component.ts
+++ src/app/pages/Home/heavy-lifting/heavy-lifting.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, Inject, PLATFORM_ID } from '@angular/core';
+import { isPlatformBrowser } from '@angular/common';
+import * as AOS from 'aos';
 
 @Component({
   selector: 'app-heavy-lifting',
   templateUrl: './heavy-lifting.component.html',
   styleUrl: './heavy-lifting.component.scss'
 })
-export class HeavyLiftingComponent {
+export class HeavyLiftingComponent implements OnInit {
+
+  constructor(@Inject(PLATFORM_ID) private platformId: Object) { }
+
+  ngOnInit(): void {
+    if (isPlatformBrowser(this.platformId)) {
+      AOS.init({
+        duration: 800,
+        easing: 'ease-in-out',
+        once: true,
+        mirror: false,
+        offset: 100
+      });
+    }
+  }
 
 }
